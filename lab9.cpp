468ad0b9b3d287603cf192b42c7fe6ddd56c5250
vlad <vlad.horbachevsky@gmail.com>
2015-02-16 19:52:41 +0300

    finished

--- lab9.cpp
+++ lab9.cpp
@@ -6,23 +6,26 @@ using namespace std;
 double maxr(double *a,int n);
 int main()
 {
-	double a[8] = {2, 3, 4, 10, 9, 30, 10, 1};
+	double a[8] = {2, 3, 4, 10, 9, 30, 10, 50};
 	double r = maxr(a, 8);
 	cout << "Maximum = " << r << endl;
 	return 0;
 }
 double maxr(double *a,int n)
 {
-	if(n==1)
+	int max1;
+	if (n == 0)
+		return a[0];
+	else
+	{
+		max1 = maxr(a, n - 1);
+	}
+	if (a[n] > max1)
 		{
-			return a[0];
+			return a[n];
 		}
-	if(n==2)
-		{
-			return max(a[0],a[1]);
-		}
-	
-	double max1 = maxr(a, n/2);
-	double max2 = maxr(a + n/2, n/2);
-	return max(max1, max2);
+	else 
+	{
+		return max1;
+	}
 }
